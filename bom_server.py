9e43b2d391c70bafc0e6b50d5c88a80ccb608ece
Jachin Rupe <jachin.rupe@gmail.com>
2015-04-17 11:16:54 -0500

    After now uses the UUID instead of the ID.

--- bom_server.py
+++ bom_server.py
@@ -37,13 +37,35 @@ class index:
 
 
 class after:
-    def GET(self, after_id):
+    def GET(self, after_uuid):
 
         upcs = []
 
-        vars = dict(id = after_id)
-        for upc in db.select('upc', vars, where="id > $id"):
-            upcs.append(upc)
+        results = db.select(
+            'upc',
+            dict(after_uuid = after_uuid),
+            where="uuid = $after_uuid"
+        )
+
+        # Find the UPC we want all the UPCs after.
+        after_upc = None
+        for upc in results:
+            after_upc = upc
+
+        if after_upc:
+            # If we find it, get all the UPCs after this one.
+            results = db.select(
+                'upc',
+                dict(after_id = after_upc.id),
+                where="id > $after_id"
+            )
+
+            for upc in results:
+                upcs.append(upc)
+        else:
+            # Can't find the 'after' UPC so just select them all.
+            for upc in db.select('upc'):
+                upcs.append(upc)
 
         web.header('Content-Type', 'application/json')
         return json.dumps(upcs, default=json_encode_handler)
